55d9ab3b3f2222d4ff9566b89b7258dab0ab61ef
Shengjie Xu <lichkingxsj@gmail.com>
2016-11-18 14:37:04 -0800

    Lay out the UI of the start page

--- RefractEasy/Start.js
+++ RefractEasy/Start.js
@@ -10,7 +10,42 @@ import {
 
 var styles = StyleSheet.create({
     container: {
-        marginTop: 65,
+        marginTop: 120,
+        padding: 30,
+        alignItems: 'center',
+    },
+    description: {
+        marginBottom: 50,
+        fontSize: 30,
+        textAlign: 'center',
+        color: '#2077E8',
+    },
+    image: {
+        width: 320,
+        height: 154,
+    },
+    flowRight: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+    },
+    smallTexts: {
+        marginTop: 50,
+        marginBottom: 50,
+        marginLeft: 10,
+        marginRight: 10,
+        fontSize: 15,
+        textAlign: 'center',
+        color: 'white',
+        backgroundColor: 'paleturquoise',
+        fontWeight: 'bold',
+        flex: 1,
+        padding: 10,
+        borderWidth: 2,
+        borderColor: 'paleturquoise',
+        textShadowColor: 'lightseagreen',
+        textShadowOffset: {width: 3, height: 3,},
+        textShadowRadius: 10,
     },
 });
 
@@ -19,7 +54,12 @@ class Start extends Component {
     render(){
         return (
             <View style={styles.container}>
-                <Text>TEST</Text>
+                <Text style={styles.description}>Press one eye to begin</Text>
+                <Image source={require('./Resources/Start-eyes.png')} style={styles.image}/>
+                <View style={styles.flowRight}>
+                    <Text style={styles.smallTexts}>Left</Text>
+                    <Text style={styles.smallTexts}>Right</Text>
+                </View>
             </View>
         );
     }
