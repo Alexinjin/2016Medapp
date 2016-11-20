ce7bdb1d062dbd440c5c1576fe372b17ddc2d340
henrychen0220 <whchen1@uci.edu>
2016-11-19 18:12:58 -0800

    Last 2 screens

---
+++ RefractEasy/LastScreen.js
@@ -0,0 +1,89 @@
+"use strict";
+
+import React, {Component} from 'react';
+import {StyleSheet, View, Text, TextInput, TouchableHighlight} from 'react-native';
+
+var styles = StyleSheet.create({
+  container: {
+      marginTop: 100,
+      padding: 30,
+      alignItems: 'center',
+  },
+  description: {
+      marginBottom: 50,
+      fontSize: 30,
+      textAlign: 'center',
+      color: '#2077E8',
+  },
+  flowRight: {
+      padding:1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      alignSelf: 'stretch',
+  },
+  dataText: {
+      padding: 5,
+      fontSize: 15,
+      textAlign: 'left',
+  },
+  flowDown: {
+      flexDirection: 'column',
+      alignItems: 'center',
+      alignSelf: 'stretch',
+  },
+  buttonText: {
+    fontSize: 15,
+    color: 'black',
+    textAlign: 'right',
+  },
+  searchInput: {
+    flex: 4,
+    paddingLeft:4,
+    fontSize: 15,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 10,
+    color: '#48BBEC'
+  },
+  button: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 3,
+    borderRadius: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+})
+
+class LastScreen extends Component{
+  render(){
+    return(
+      <View style = {styles.container}>
+        <Text style = {styles.description}>Confirmation</Text>
+        <View>
+          <Text style = {styles.dataText}>1. Sphere Power Refinement should be close</Text>
+          <Text style = {styles.dataText}>2. Fogged again by adding 0.75</Text>
+          <Text style = {styles.dataText}>3. Subtract 0.25 from the patient</Text>
+          <Text style = {styles.dataText}>4. Ask if the vision is better</Text>
+          <Text style = {styles.dataText}>5. Stop if no more appreciably improvement</Text>
+          <View style = {styles.flowRight}>
+            <TextInput
+            style = {styles.searchInput}
+            placeholder = ' Patient Stops At: '/>
+            <TouchableHighlight
+              style = {styles.button}
+              uderlayColor = 'blue'>
+              <Text style = {styles.buttonText}>Confirm</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+
+
+module.exports = LastScreen;
